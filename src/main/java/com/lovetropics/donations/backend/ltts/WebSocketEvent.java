67137cad85741ffe1564d742694e5407a7b7570f
Gegy <me@gegy.dev>
2025-11-17 16:46:56 +0100

    Use profile id from techstack in whitelist events (if present)

--- src/main/java/com/lovetropics/donations/backend/ltts/WebSocketEvent.java
+++ src/main/java/com/lovetropics/donations/backend/ltts/WebSocketEvent.java
@@ -7,6 +7,7 @@ import com.lovetropics.lib.permission.PermissionsApi;
 import com.lovetropics.lib.permission.role.Role;
 import com.lovetropics.lib.techstack.Crud;
 import com.lovetropics.lib.techstack.TechstackEventSubscriber;
+import com.mojang.authlib.GameProfile;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import net.minecraft.Util;
@@ -18,6 +19,7 @@ import org.slf4j.Logger;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
@@ -30,35 +32,47 @@ public class WebSocketEvent<T> {
             .on(Crud.CREATE, DonationHandler::queueDonation);
 
     public static final WebSocketEvent<WhitelistEvent> WHITELIST = register("whitelist", WhitelistEvent.CODEC)
-            .on(Crud.CREATE, e -> {
-                if (!StringUtil.isValidPlayerName(e.name())) {
-                    LOGGER.warn("Ignoring requested whitelist for invalid player name: {}", e.name());
+            .on(Crud.CREATE, event -> {
+                if (!StringUtil.isValidPlayerName(event.profileName())) {
+                    LOGGER.warn("Ignoring requested whitelist for invalid player name: {}", event.profileName());
                     return;
                 }
                 MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-                CompletableFuture.supplyAsync(() -> server.getProfileCache().get(e.name()), Util.backgroundExecutor())
-                        .thenAcceptAsync(profile -> {
-                            if (profile.isEmpty()) {
-                                LOGGER.warn("Ignoring requested whitelist for profile that does not exist: {}", e.name());
-                                return;
-                            }
-                            String roleId = DonationConfigs.WHITELIST.role.get();
-                            Role role = PermissionsApi.provider().get(roleId);
-                            if (role == null) {
-                                LOGGER.warn("No role exists with id: {}, ignoring whitelist request for {} for now", roleId, e.name());
-                                return;
-                            }
-                            if (e.type() == WhitelistEvent.Type.WHITELIST) {
-                                LOGGER.info("Adding {} role to {} ({})", role, profile.get().getName(), profile.get().getId());
-                                PermissionsApi.modifier().addRoleTo(profile.get().getId(), role);
-                            } else if (e.type() == WhitelistEvent.Type.BLACKLIST) {
-                                LOGGER.info("Removing {} role to {} ({})", role, profile.get().getName(), profile.get().getId());
-                                PermissionsApi.modifier().removeRoleFrom(profile.get().getId(), role);
-                            }
-                            Util.nonCriticalIoPool().execute(() ->
-                                    DonationRequests.get().ackWhitelist(e.name(), e.type())
-                            );
-                        }, server);
+                CompletableFuture<Optional<GameProfile>> profileFuture;
+                if (event.profileId().isPresent()) {
+                    profileFuture = CompletableFuture.completedFuture(Optional.of(
+                            new GameProfile(event.profileId().get(), event.profileName())
+                    ));
+                } else {
+                    // The techstack might fail its player name lookup - but we'll retry until it's successful
+                    // TODO: Eventually we should ensure that the techstack never fails and only sends fully formed whitelist events
+                    profileFuture = CompletableFuture.supplyAsync(
+                            () -> server != null ? server.getProfileCache().get(event.profileName()) : Optional.empty(),
+                            Util.nonCriticalIoPool()
+                    );
+                }
+                profileFuture.thenAcceptAsync(profile -> {
+                    if (profile.isEmpty()) {
+                        LOGGER.warn("Ignoring requested whitelist for profile that does not exist: {}", event.profileName());
+                        return;
+                    }
+                    String roleId = DonationConfigs.WHITELIST.role.get();
+                    Role role = PermissionsApi.provider().get(roleId);
+                    if (role == null) {
+                        LOGGER.warn("No role exists with id: {}, ignoring whitelist request for {} for now", roleId, event.profileName());
+                        return;
+                    }
+                    if (event.type() == WhitelistEvent.Type.WHITELIST) {
+                        LOGGER.info("Adding {} role to {} ({})", role, profile.get().getName(), profile.get().getId());
+                        PermissionsApi.modifier().addRoleTo(profile.get().getId(), role);
+                    } else if (event.type() == WhitelistEvent.Type.BLACKLIST) {
+                        LOGGER.info("Removing {} role to {} ({})", role, profile.get().getName(), profile.get().getId());
+                        PermissionsApi.modifier().removeRoleFrom(profile.get().getId(), role);
+                    }
+                    Util.nonCriticalIoPool().execute(() ->
+                            DonationRequests.get().ackWhitelist(event.profileName(), event.type())
+                    );
+                }, server);
             });
 
     private static <T> WebSocketEvent<T> register(String key, Codec<T> type) {
@@ -79,6 +93,15 @@ public class WebSocketEvent<T> {
             throw new IllegalArgumentException("Cannot add duplicate event action handler for " + action + " on event " + codec);
         }
         return this;
+    }
+
+    private WebSocketEvent<T> onServerThread(Crud action, Consumer<T> callback) {
+        return on(action, event -> {
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            if (server != null) {
+                server.execute(() -> callback.accept(event));
+            }
+        });
     }
 
     public void act(Crud action, T payload) {
--- src/main/java/com/lovetropics/donations/backend/ltts/json/WhitelistEvent.java
+++ src/main/java/com/lovetropics/donations/backend/ltts/json/WhitelistEvent.java
@@ -2,12 +2,18 @@ package com.lovetropics.donations.backend.ltts.json;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.util.StringRepresentable;
 
-public record WhitelistEvent(Type type, String name) {
+import java.util.Optional;
+import java.util.UUID;
+
+public record WhitelistEvent(Type type, String profileName, Optional<UUID> profileId) {
     public static final Codec<WhitelistEvent> CODEC = RecordCodecBuilder.create(i -> i.group(
             Type.CODEC.fieldOf("type").forGetter(WhitelistEvent::type),
-            Codec.STRING.fieldOf("name").forGetter(WhitelistEvent::name)
+            Codec.STRING.fieldOf("name").forGetter(WhitelistEvent::profileName),
+            // No ID is sent as an empty string if the techstack fails to fetch it
+            UUIDUtil.AUTHLIB_CODEC.lenientOptionalFieldOf("uuid").forGetter(WhitelistEvent::profileId)
     ).apply(i, WhitelistEvent::new));
 
     public enum Type implements StringRepresentable {
